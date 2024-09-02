5e3bae126530c5317f8a84bb892f270605ba9d05
clara seijo <clasei@proton.me>
2024-09-02 22:43:54 +0200

    iteration 1: soldier done

--- src/viking.js
+++ src/viking.js
@@ -1,5 +1,20 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  receiveDamage(damage) {
+    this.damage = damage;
+
+    this.health = this.health - this.damage;
+  }
+}
 
 // Viking
 class Viking {}
